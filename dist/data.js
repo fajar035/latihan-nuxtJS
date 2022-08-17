53a3d2661ed9d88b1099606b9aa720ada762ed5c
ikehikeh151 <ikehikeh151@gmail.com>
2022-08-18 04:58:44 +0700

    build: build

---
+++ dist/data.js
@@ -0,0 +1,87 @@
+const data = {
+  testimoni: [
+    {
+      name: "Rico Andrian",
+      jabatan: "Fullstack Engineer di Rey.id",
+      ig: "@rpetz__",
+      foto: "rico-andrian.5427f9c.png",
+      quotes:
+        '"Mulai dari nol", pasti kita sering dengar kalimat itu. Nah itu yang saya rasakan disini. Masuk dengan tangki kosong lalu keluar dengan tangki yang terisi. Dengan mentor yang family friendly, pengajaran yang baik, dan teman - teman yang asik sangat mendukung proses belajar saya. Tak hanya teknikal namun softskill juga saya dapatkan. Terakhir, program ISA yang sangat membantu saya. So, let`s get it on.',
+    },
+    {
+      name: "Irfan Junaidi",
+      jabatan: "Software Engineer di PT. Anak Muda Digital",
+      ig: "@irvan_junjun",
+      foto: "irfan-junaidi.e694b62.png",
+      quotes:
+        "Selama pelatihan di bootcamp fazztrack ini, saya mendapatkan banyak manfaat. Dari skill teknikal dan juga soft skill, disana juga mendapatkan pengalaman yang sangat banyak, dari pengalaman project tim, hingga mengerjakan suatu project dengan jangka waktu yang tidak banyak tentunya. Selain itu, didalam pelatihan ini mendapatkan relasi baru dan tentunya ilmu yang sangat bermanfaat.",
+    },
+    {
+      name: "Soultan Muhammad Albar",
+      jabatan: "Frontend Engineer di Dagangan",
+      ig: "@soultan.muh",
+      foto: "soultan-muhammad-albar.b30a3e7.png",
+      quotes:
+        "Belajar di sini selama 6 minggu jaman sebelum corona, fasilitasnya mantap, terutama wifinya yang kenceng banget. Gak ada 2 minggu setelah selesai bootcamp, langsung dapet kerja dengan gaji yang mantap (padahal saya lulusan sma wkw). Skill yang didapat baik hard maupun soft skill sampai sekarang masih terpakai dengan baik, terutama waktu ambil projek2 freelance",
+    },
+    {
+      name: "Azmi Prilly Naisa",
+      jabatan: "Software Engineer di Bank BTPN",
+      ig: "@azmprllynsa",
+      foto: "azmi-prilly-naisa.69d9442.png",
+      quotes:
+        'Setahun lalu join course disini dan asli, game changer banget. Trainernya baik - baik, keren - keren dan super helpfull bahkan setelah lulus udah lama banget. Waktu baru lulus, cari kerja mentok banget di interview, dengan penuh kebingungan "apa yang harus gue jual dari diri gue" Tapi after join course disini tuh... Ada lahhh yang bisa dijual wkwkwk dan bisa dapet banyak offer sampai detik ini. Thankyou Fazztrack',
+    },
+  ],
+  pilihan: [
+    {
+      name: "Fullstack Mobile",
+      description:
+        "Mempelajari bagaimana membangun aplikasi mobile dengan Javascript dari tahap perancangan selama 3 bulan pembelajaran secara online.",
+      foto: "Asset_FullMobile.82da6c4.png",
+    },
+    {
+      name: "Fullstack Website",
+      description:
+        "Mempelajari bagaimana membangun aplikasi website dari tahap perancangan sampai testing selama 3 bulan pembelajaran secara online.",
+      foto: "Web Fullstack Developer.29cedfe.png",
+    },
+    {
+      name: "Backend Javaspring",
+      description:
+        "Mempelajari bagaimana membangun aplikasi fullstack dengan React serta menggunakan Spring Boot sebagai backend selama 3 bulan pembelajaran secara online.",
+      foto: "backend-javaspring.2241d71.png",
+    },
+    {
+      name: "Backend Golang",
+      description:
+        "Mempelajari bagaimana membangun aplikasi fullstack dengan Javascript dan Golang serta otomasi sistem untuk membantu proses development lebih cepat.",
+      foto: "DevOps-Engineer-new.394e12c.png",
+    },
+  ],
+  keunggulan: [
+    {
+      name: "Kurikulum Yang Mendukung",
+      description:
+        "Kurikulum yang disusun sesuai dengan standar industri yang dapat dipelajari oleh talent dengan background apapun. Dibimbing langsung oleh mentor yang berpengalaman.",
+      icon: "kurikulum.svg",
+      link: "Daftar Sekarang",
+    },
+    {
+      name: "Disalurkan Kerja",
+      description:
+        "Selain menjamin lulus bootcamp, Fazztrack menjamin lulusan disalurkan kerja dengan dibekali pelatihan hard skill dan soft skill serta konsultasi karir untuk mendukung kesuksesan karir.",
+      icon: "salurkan.svg",
+      link: "Pelajari Lebih Lanjut",
+    },
+    {
+      name: "Income Share Agreement",
+      description:
+        "Fazztrack mempunyai program Income Share Agreement dimana lulusan tak perlu membayar sebelum diterima kerja dan lulusan akan mendapat gaji minimal sesuai standard yang sudah ditentukan.",
+      icon: "income.svg",
+      link: "Pelajari Lebih Lanjut",
+    },
+  ],
+};
+
+export default data;
